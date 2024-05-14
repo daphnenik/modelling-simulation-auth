0796d328478478efe080596ac6e71aca51ceb42d
Dafni Nikolaidou <98471114+daphnenik@users.noreply.github.com>
2024-05-14 13:18:52 +0300

    Update getV.m

--- Assignment 1/rlc/getV.m
+++ Assignment 1/rlc/getV.m
@@ -1,1 +1,19 @@
+function [VcReal,VrReal] = getV(time,N,flag)
 
+VrReal = zeros(1,N);
+VcReal = zeros(1,N);
+[VrReal,VcReal]=v(time);
+
+% getting some wrong values to observe the model's behaviour
+if (flag == 1)
+% random values between 50 and 100
+a = 10;
+b = 50;
+
+ for i=1:5
+     VcReal(:,randi(N)) = (b-a)*rand() + a;
+     VrReal(:,randi(N)) = (b-a)*rand() + a;
+ end
+end
+
+end
